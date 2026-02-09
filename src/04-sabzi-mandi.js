7204d8f63ed2afb21c27afcc43dd54c8c46641ef
Satpal777 <rsshacker777@gmail.com>
2026-02-09 22:43:01 +0530

    Comp: 04: Implement Sabzi Cart

--- src/04-sabzi-mandi.js
+++ src/04-sabzi-mandi.js
@@ -48,21 +48,27 @@
  *   removeLastItem(["tamatar", "pyaaz", "mirchi"])   // => "mirchi"
  */
 export function addToCart(cart, item) {
-  // Your code here
+  if (!Array.isArray(cart)) return -1;
+  if (typeof item === 'string' && item.length) cart.push(item);
+  return cart.length;
 }
 
 export function addUrgentItem(cart, item) {
-  // Your code here
+  if (!Array.isArray(cart)) return [];
+  if (typeof item === 'string' && item.length) cart.unshift(item);
+  return cart;
 }
 
 export function removeLastItem(cart) {
-  // Your code here
+  if (!Array.isArray(cart)) return;
+  return cart.pop();
 }
 
 export function isInCart(cart, item) {
-  // Your code here
+  if (!Array.isArray(cart)) return false;
+  return cart.includes(item);
 }
 
 export function mergeCarts(cart1, cart2) {
-  // Your code here
+  return ((Array.isArray(cart1) ? cart1 : []).concat(Array.isArray(cart2) ? cart2 : []));
 }
